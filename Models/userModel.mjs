f62c62bec899b6764a0d56afffee4c40b9eb4fad
fmilheir <contada42@gmail.com>
2023-10-31 09:48:57 -0700

    premilinary API databese with simple test for user

--- Models/userModel.mjs
+++ Models/userModel.mjs
@@ -12,6 +12,7 @@ class userModel{
     };
 
     async addUser(username, password, permission_level) {
+      console.log(` this is my ${username}`)
         try {
           const query = 'INSERT INTO users (username, password, permission_level) VALUES (?, ?, ?)';
           const [result] = await pool.query(query, [username, password, permission_level]);
--- test/serverCheck.test.mjs
+++ test/serverCheck.test.mjs
@@ -4,33 +4,33 @@ import userController from '../Controllers/userControl.mjs';
 import PointsOfInterestModel from '../Models/poiModel.mjs';
 import PointsOfInterestController from '../Controllers/poiControl.mjs';
 
-describe('User Model and Controller Tests', () => {
-  it('should retrieve a user by username', async () => {
-    const userModel = new UserModel();
-
-    // Insert a test user into the database
-    const testUser = {
-      username: 'testuser',
-      // Add other user properties here
-    };
-
-    const result = await userModel.addUser(testUser);
-
-    // Test the getUserByUsernameController
-    const req = { params: { username: 'testuser' } };
-    const res = {
-      json: (data) => {
-        assert.strictEqual(data.username, 'testuser');
-      },
-      status: (code) => {
-        assert.strictEqual(code, 200);
-        return res;
-      },
-    };
-
-    await UserController.getUserByUsernameController(req, res);
-
-    // Clean up: delete the test user
-    await userModel.deleteUser(result.userId);
-  });
+jest.mock('../public/scripts/pool.mjs', () => {
+  const mockQuery = jest.fn();
+  return {
+    query: mockQuery,
+  };
 });
+
+describe('User Model Tests', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should add a user', async () => {
+    const userModel = new UserModel();
+    const mockInsertId = 1; // Adjust with your expected insert ID
+
+    // Mock the query function to return a result
+    const pool = require('../public/scripts/pool.mjs');
+    pool.query.mockResolvedValueOnce([{ insertId: mockInsertId }]);
+
+    const result = await userModel.addUser('testuser', 'password', 'admin');
+
+    expect(pool.query).toHaveBeenCalledWith(expect.any(String), ['testuser', 'password', 'admin']);
+    expect(result).toBe(mockInsertId);
+  });
+
+  // Other test cases follow similarly...
+});
+
+
