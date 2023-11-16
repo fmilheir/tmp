4eef39c4f8b1bf91bbdcbff753b1059bf9c1b1ce
fmilheir <contada42@gmail.com>
2023-11-15 16:46:46 -0800

    new

--- public/scripts/server.mjs
+++ public/scripts/server.mjs
@@ -1,32 +1,71 @@
 import express from "express";
 import cors from  'cors';
-import userRoute from '../../routes/userRoute';
+import userRoute from '../../routes/userRoute.mjs';
 import path from 'path';
 import { fileURLToPath } from "url";
-//import i18next from "i18next";
+import session from 'express-session';
+import startSession from './session.mjs';
+import bodyParser from 'body-parser';
+//import i18next from "i18next";ee
 //import setLanguage from "../../middleware/setLanguage.mjs";
 
 
-const app = express();
+// ... (your imports)
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-/*app.use(i18n.middleware.handle(i18next));
-app.use(setLanguage);*/
-app.use('/user', userRoute);
-//app.use(express.static("publid"));
 app.use("/public", express.static('./public/'));
 
+// Configure the MySQL session store
+const sessionStore = startSession.startSession;
 
+// Define the session middleware for specific routes
+app.use('/user/login', session({
+  secret: 'Mysupersecret',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: true,
+  unset: 'destroy',
+  proxy: true,
+  cookie: {
+    maxAge: 600000,
+    httpOnly: false,
+  },
+}));
+
+// Define a middleware to check if a user is authenticated
+app.use((req, res, next) => {
+  if (req.path === "/public/login" || req.path === "/login" || req.path === "/user/login") {
+    // Allow access to public pages and login-related routes
+    console.log("Public page");
+    next();
+  } else if (req.session && req.session.username) {
+    // The user is logged in, and you can access req.session.username
+    console.log("Authenticated");
+    console.log(`Logged in as: ${req.session.username}`);
+    next();
+  } else {
+    // The user is not logged in or accessing a protected page
+    console.log("Forbidden");
+    return res.status(401).json({ error: "You're not logged in. Go away!" });
+  }
+});
+
+app.use('/user', userRoute);
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.redirect( "/public/index.html");
 });
 
-app.get("/login", (req, res) => {
-    res.redirect( "/public/login.html"); 
-});
+app.post("/login", (req, res) => {
+    // Set the 'username' in the session
+    req.session.username = req.body.username;
+    console.log(`this is my usernadasdaame: ${req.session.username}`);
+    res.redirect("/public/login.html");
+  });
 
 app.get("/verify", (req, res) =>{
     res.redirect("/public/verify.html");
@@ -60,6 +99,11 @@ app.get("/verificationcode", (req, res) => {
     res.redirect(`/public/verificationcode.html?code=${verificationCode}`);
 });
 
+
+app.get("/welcome", (req, res) => {
+    res.redirect("/public/welcome.html");
+});
+
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!")
 });
@@ -69,5 +113,6 @@ app.use((err, req, res, next) => {
     res.status(500).send({ error: err.message || 'Something broke!' });
 
 });
+
 
 export default app;
