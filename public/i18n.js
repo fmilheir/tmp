5addc37726262d3f5283aaac13cd8c9b8ea97731
MCoding90 <czerwonkaster@gmail.com>
2023-11-17 18:26:38 +0000

    set up for i18n done

--- public/i18n.js
+++ public/i18n.js
@@ -1,15 +1,32 @@
-import i18next from "i18next";
+import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { LanguageDetector } from "i18next-browser-languagedetector";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { DateTime } from "luxon";
 
-i18next
-	.use(initReactI18next)
+i18n
+	// i18next-http-backend
+	// loads translations from the server
+	.use(Backend)
+	// detect user language
 	.use(LanguageDetector)
+	// pass the i18n instance to react-i18next.
+	.use(initReactI18next)
+	// init i18next
 	.init({
+		debug: true,
 		fallbackLng: "en",
-		resources: {
-			en: {
-				translation: {},
+		interpolation: {
+			escapeValue: false, // not needed for react as it escapes by default
+			format: (value, format, lng) => {
+				if (value instanceof Date) {
+					return DateTime.fromJSDate(value)
+						.setLocale(lng)
+						.toLocaleString(DateTime[format]);
+				}
+				return value;
 			},
 		},
 	});
+
+export default i18n;
--- public/scripts/server.mjs
+++ public/scripts/server.mjs
@@ -1,34 +1,56 @@
 import express from "express";
-import cors from  'cors';
-import userRoute from '../../routes/userRoute.mjs';
+import cors from "cors";
+import userRoute from "../../routes/userRoute.mjs";
 
+import { Suspense } from "react";
+//import { useTranslation } from "react-i18next";
+
+const lngs = {
+	en: { nativeName: "English" },
+	de: { nativeName: "Deutsch" },
+};
+
+/*
+function App() {
+	const { t } = useTranslation();
+	return (
+		
+	)
+};
+
+// here app catches the suspense from page in case translations are not yet loaded
+export function WrappedApp() {
+	return (
+		<Suspense fallback="...is loading">
+			<App />
+		</Suspense>
+	);
+}*/
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/user', userRoute);
+app.use("/user", userRoute);
 app.use(express.static("publid"));
-app.use("/public", express.static('./public/'));
-
+app.use("/public", express.static("./public/"));
 
 app.get("/", (req, res) => {
-    res.redirect( "/public/index.html"); // only this one works 
+	res.redirect("/public/index.html"); // only this one works
 });
 
 app.get("/login", (req, res) => {
-    res.redirect( "/public/login.html"); 
-})
+	res.redirect("/public/login.html");
+});
 
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!")
+	res.status(404).send("Sorry can't find that!");
 });
 
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
 });
 
 export default app;
