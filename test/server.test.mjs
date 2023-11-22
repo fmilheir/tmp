2edcc7a027f36e1f6f6c4004eb6ffc0a6a4f9a49
Oladapo Giwa-Daramola <91552016+Oladapo01@users.noreply.github.com>
2023-11-22 11:40:21 +0000

    Update server.test.mjs

--- test/server.test.mjs
+++ test/server.test.mjs
@@ -1,14 +1,13 @@
-
 import request from 'supertest';
 import app from '../public/scripts/server.mjs'; 
 
 describe('Express server', () => {
-  /*it('Should redirect to the /public/index.html on GET /', async () => {
+  it('Should redirect to the /public/index.html on GET /', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(302);
     // Depending on how your express.static is set up, this might just be '/index.html'
     expect(response.header['location']).toEqual('/public/index.html');
-  });*/
+  });
 
   // The server should return a 404 status code when a GET request is made to a nonexistent URL.
   it('Should return 404 on GET /non-existent-path', async () => {
@@ -21,4 +20,4 @@ describe('Express server', () => {
     const response = await request(app).get('/public/index.html');
     expect(response.status).toBe(200);
   });
-
+});
